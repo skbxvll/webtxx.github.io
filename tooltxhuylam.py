1ff8a5ef465f724cff4032c21eab7a69ab72f6b8
skbxvll <166694336+skbxvll@users.noreply.github.com>
2024-04-11 14:25:10 +0700

    Add files via upload

---
+++ tooltxhuylam.py
@@ -0,0 +1,23 @@
+print("* Luu Y : ti le khong phai 100%")
+print("CACH THEO TOOL CUA HUY LO")
+print("NEU KET QUA XUAT HIEN GIONG NHAU KET QUA CHI DUNG TOI DA 3 LAN !")
+print("Khong nen tat tay!")
+print(" truon hop 3 con xuc xac trung nhau kq co the sai , ban can cho van sau")
+print("VD: RA 1-1-1 , 2-2-2...")
+print(" truong hop : THUA LIEN TUC 4 VAN TRO LEN THI CAN BO 5 VAN SAUU VI DO LA LUC CAI BE ")
+
+print(" ----------TOOL TX CUA HUYLO----------")
+
+n = int(input(" Nhap ket qua cua phien vua roi :   "))
+x= (n + n/100*50)
+y=x/2
+c=y
+rounded_c = round(c)
+print(rounded_c)  # Output: 3
+u=rounded_c
+print("Số KẾT QUAT : " ,u)
+print("SỐ CHẴN = TÀI")
+print('Số lẻ = XỈU')
+print ("TỈ LỆ THUA 12%")
+ 
+           
